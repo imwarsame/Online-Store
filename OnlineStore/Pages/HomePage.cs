4cb72395aa9bf29bd7a5a720fe28eeadd319960e
Warsame Ali <46595705+imwarsame@users.noreply.github.com>
2019-06-09 13:47:17 +0100

    Update HomePage.cs
    add comments

--- OnlineStore/Pages/HomePage.cs
+++ OnlineStore/Pages/HomePage.cs
@@ -9,7 +9,18 @@ namespace OnlineStore.Pages
         private IWebDriver driver;
 
         //init our page and elements
-       
+       /* Below constructor was refactored into the InitThisPage class
+       public HomePage(IWebDriver driver)
+       {
+     
+          ---this.driver refers to the private instance field delcared on line 9 of this class
+          ---when we create a new HomePage object, in our tests,we pass in the driver i.e. Chrome or Firefox
+          ---and PageFactory.InitElement will initialise all the elements on the choosen driver
+        
+          this.driver = driver;
+          PageFactory.InitElements(driver, this);
+        }
+       */
 
         //Elements -  the Page Factory way
         [FindsBy(How = How.Id, Using = "account")]
